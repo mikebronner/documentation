e2d0c35b015e777389f55e578545bcb4bf643e77
Mike Bronner <mike@genealabs.com>
2019-10-31 16:30:05 -0700

    Fix main menu colors.

--- source/_nav/menu-item.blade.php
+++ source/_nav/menu-item.blade.php
@@ -2,7 +2,7 @@
     @if ($url = is_string($item) ? $item : $item->url)
         {{-- Menu item with URL--}}
         <a href="{{ $page->url($url) }}"
-            class="{{ 'lvl' . $level }} {{ $page->isActiveParent($item) ? 'lvl' . $level . '-active' : '' }} {{ $page->isActive($url) ? 'active font-semibold text-green-600' : '' }} nav-menu__item hover:text-green-900"
+            class="{{ 'lvl' . $level }} {{ $page->isActiveParent($item) ? 'lvl' . $level . '-active' : '' }} {{ $page->isActive($url) ? 'active font-semibold' : '' }} nav-menu__item text-green-600 hover:text-green-900"
         >
             {{ $label }}
         </a>
