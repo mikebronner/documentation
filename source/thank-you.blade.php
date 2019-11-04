c2ea2e564e3b1a351ee85c3886480e3839b7e4e9
Mike Bronner <mike@genealabs.com>
2019-11-04 08:46:29 -0800

    Add thank-you page for contact form.

---
+++ source/thank-you.blade.php
@@ -0,0 +1,12 @@
+@extends('_layouts.master')
+
+@section('content')
+    <section class="container mx-auto py-6 px-4">
+        <h1>Contact</h1>
+
+        <p class="mb-8">
+            Thank you for reaching out. We will respond as soon as possible,
+            usually within one or two business days.
+        </p>
+    </section>
+@stop
