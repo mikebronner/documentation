6138318096705d92d9fafe69ed66b2ea785e5194
Mike Bronner <mike@genealabs.com>
2020-05-20 13:40:35 -0700

    Add initial version of branded templates page.

---
+++ source/branded-presentation-template.blade.php
@@ -0,0 +1,177 @@
+@extends('_layouts.master')
+
+@push ('scripts')
+    <script
+        type="text/javascript"
+        src="https://genealabs.17hats.com/ruby/embed/lead/script/hrspbdcvncvnbdpkngrcrxnrwsbghcws"
+    ></script>
+@endpush
+
+@push ('styles')
+
+@endpush
+
+@section('content')
+    <section class="container mx-auto max-w-2xl">
+        <h1>Branded Presentation Templates</h1>
+        <h2>Frustrated with your slideshows?</h2>
+        <p>
+            You give presentations, host webinars, or create online courses. You
+            feel like you are re-inventing the wheel each time you create a new 
+            presentation. Maybe your branding isn't consistent from one to the
+            other or your designs seem unprofessional. You know its something
+            you need to fix, but you don't know how and don't have the time to
+            figure it out for yourself. Have you been putting this off, because
+            it turned out to be a lot more work than expected?
+        </p>
+        <p>
+            Powerpoint and Keynote make it easy to create presentations: after
+            all, you only need to drag-and-drop the content onto the slides.
+            However, this can be a slippery slope, as it can encourage not being
+            picky about branding (your logo, colors, fonts, etc.) and design (layout,
+            styling, spacing, use of colors, etc.). All these aspects take time
+            to learn and implement. Time you don't have because you're busy
+            running your business.
+        </p>
+        <h2>Imagine Having It Already Done</h2>
+        <p>
+            Imagine (no seriously, visualize your slideshow) having your
+            custom-branded templates ready to be filled in. Imagine not having
+            to worry about layout and design each time your fire up Powerpoint
+            or Keynote. Imagine being able to create your uniquely branded
+            slideshow in a fraction of the time it used to take.
+        </p>
+        <h2>I'll Take Care Of This For You</h2>
+        <p>
+            Focus on presenting, on running your business. We will handle the
+            design and branding of your slideshow template. With this new
+            template you will be able to assemble your presentations quicker,
+            appear more professional, and be focussed on earning money, rather
+            than having to learn technologies that are not directly in line with
+            your business. Order one of our slideshow template packages now. Why?
+        </p>
+        <ul>
+            <li>I have years of experience with slideshows and Audio/Visual technologies.</li>
+            <li>I understand design and accessiblity.</li>
+            <li>I have successfully helped other professional genealogists with this.</li>
+        </ul>
+        <h3>Here Is How It Works</h3>
+        <p>
+            You first choose what level of package you would be interested in,
+            anywhere from 6 to 18 slide formats.
+        </p>
+        <p>
+            I'll ask you for your branding materials (typefaces, colors, logos),
+            as well as any supporting materials like pamphlets, web sites,
+            handouts, that you have done in the past as a branding reference. I
+            would also ask that you provide your most complex presentation you
+            have put together. We will use that as a benchmark during design (to
+            guide us in designing your slides based on how you use them). You
+            will also receive that presentation implemented using the new
+            template.
+        </p>
+        <p>
+            After receiving your materials, I will work on your template and
+            provide you with a first draft within 5 working days for your
+            review. You will have 1 round of design changes based on your
+            feedback, if necessary. Pending your approval, we will send you the
+            final product.
+        </p>
+        <h2>Sample Slide Formats</h2>
+        <p>
+            The template consists of some or all of the following types of
+            slides. Please note that layouts may differ on your template.
+        </p>
+        <p>===> example slide images here <===</p>
+        <h2>Are You Ready To Get Started?</h2>
+        <p>
+            Are you ready to move your presentations to the next level? Great!
+            Order your template here. The pricing is between $400 and $1200,
+            depending on the package you choose. Professional templates show
+            that you are serious about your business, and the cost is easily
+            recouped through increased business and/or higher fees.
+        </p>
+        <button
+            class="m-4 mx-auto py-2 px-6 block text-xl rounded-lg bg-green-800 text-green-100"
+        >
+            Order Now
+        </button>
+        <h2>100% Money-Back Guarantee</h2>
+        <p>
+            We are confident that you will find the templates you receive
+            helpful in speeding up your presentation development process, and
+            the design professional, clean, and on-point to your branding. If
+            after reviewing the designs you feel that I am not delivering a
+            valuable product, please let me know, and I will happily refund your
+            payment in full.
+        </p>
+        <h2>Still have a question or two?</h2>
+        <h3>What happens after I place the order?</h3>
+        <p>
+            You will receive an invoice from GeneaLabs with further details and
+            options to choose from. Based on your choices you will be presented
+            with an invoice for the appropriate amount.
+        </p>
+        <h3>What happens after I pay?</h3>
+        <p>
+            Once you submit payment, we will schedule your project in our
+            calendar and you will receive a questionaire to gather all the materials
+            necessary to create your branded template. These required materials
+            include brand details (logo, colors, typefaces) and the most
+            comprehensive presentation you have built in the past.
+        </p>
+        <h3>What happens after I submit the materials?</h3>
+        <p>
+            This is where I start working on your branded templates! Within 5
+            business days we will provide a preview of the design. At this point
+            you will have the opportunity to review the designs and provide
+            feedback. I then incorporate your feedback and present the final
+            designs. If you approve the designs, you will receive the fully
+            functional template.
+        </p>
+        <h3>What happens if I don't like the final design?</h3>
+        <p>
+            At this point you have two options: we can go through additional
+            design iterations for a nominal fee for each additional iteration.
+            If you feel that I have completely missed the mark and don't think
+            the design is salvagable, you can request a refund. In that case I
+            will refund your payment in full, no questions asked. However, you
+            will not receive the template we worked on.
+        </p>
+        <h3>How long does the project take from start to finish?</h3>
+        <p>
+            From the time we start working on the template to final approval,
+            including one design iteration, you could expect the project to take
+            about two weeks.
+        </p>
+        <h3>I still have additional questions, though...</h3>
+        <p>
+            No worries! Simply email me at
+            <a href="mailto:mike@genealabs.com">mike@genealabs.com</a>.
+        </p>
+        <h2>About Me</h2>
+        <p>
+            I'm Mike Bronner, co-partner of GeneaLabs with my wife, Myelene.
+            I've been doing web development since the mid-90s. I have also been
+            praciticing genealogy more seriously since that time as well. With
+            GeneaLabs we merge these two passions and provide professional
+            genealogists with technological solutions to help them run their
+            businesses. 
+        </p>
+        <h2>Get Scheduled Now</h2>
+        <p>
+            We schedule our clients on a first-come, first-serve basis. Be sure
+            to order now to get the process started: the sooner you order, the
+            sooner you will receive your templates for your next presentation.
+        </p>
+        <button
+            class="m-4 mx-auto py-2 px-6 block text-xl rounded-lg bg-green-800 text-green-100"
+        >
+            Order Now
+        </button>
+        <p>
+            My 100% money-back guarantee: if during the final review you aren't
+            satisfied with the results, I'll refund your payment in full.
+        </p>
+    </section>
+@endsection
